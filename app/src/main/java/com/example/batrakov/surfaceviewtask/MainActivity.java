f16cb65175db08acbce3bf1d243c027700ff2858
batrakov <phoenixed.451@gmail.com>
2017-11-24 16:45:09 +0300

    init commit

---
+++ app/src/main/java/com/example/batrakov/surfaceviewtask/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.batrakov.surfaceviewtask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+        findViewById(R.id.startButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DialogView().show(getFragmentManager(), "Dialog");
+            }
+        });
+    }
+}
